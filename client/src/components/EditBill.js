e635004a3fcef670482b69572da9eec98eb96e7b
Cesar_Cervantes <ces_29_1995@hotmail.com>
2021-03-18 20:49:09 -0600

    Create useFetchBill hook

--- client/src/components/EditBill.js
+++ client/src/components/EditBill.js
@@ -1,110 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Link, Redirect } from 'react-router-dom';
+import useFetchBill from '../hooks/useFetchBill';
 import { parseTotal } from '../utils/helperFunction';
 import BillCard from './BillCard';
 import Menu from './Menu';
 
 
 const EditBill = ({ match, history}) => {
-    const [billID, setBillID] = useState(match.params.id);
-    const [redirect, setRedirect] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
-    const [bill, setBill] = useState({
-        date: match.params.date,
-        products: [],
-        billName: '',
-        billTotal: 0,
-        isClosed: false
-    });
-    useEffect(()=>{
-        let total = parseTotal(bill.products);
-        setBill(prevBill=>({
-            ...prevBill,
-            billTotal: total
-        }));
-    }, [bill.products]);
-    useEffect(()=>{
-        const fetchBill = async()=>{
-            let date = match.params.date;
-            setIsLoading(true);
-            try {
-                let response = await fetch(`/api/v1/ventas/days/${date}/cuentas/${billID}`);
-                let { data } = await response.json();
-                console.log(data);
-                setIsLoading(false);
-                setBill(data);
-            } catch (error) {
-                setIsLoading(false);
-                setError('No se pudieron obtener los datos. Refresque la página.')
-                console.error(error);
-            }
-        };
-        if(billID !== 'nueva') fetchBill();
-    }, []);
-    const addProduct = ({price, name})=>{
-        setError('');
-        setBill(prevBill=>{
-            let newProducts = prevBill['products'].slice();
-            if(newProducts.some(product=> product.name === name)){
-                newProducts.forEach(product=>{
-                    if(product.name === name){
-                        product.quantity++;
-                    };
-                });
-            }else{
-                newProducts = [...newProducts, {name, price, quantity: 1}];
-            };
-            return {...prevBill, products: newProducts};
-        });
-    };
-    const substractProduct =  (name) => {
-        if (bill['products'].some(product => product.name === name)) {
-            setBill(prevBill => {
-                let newProducts = prevBill['products'].slice();
-                let flag = false;
-                newProducts.forEach(product => {
-                    if (product.name === name) {
-                        product.quantity--;
-                    };
-                    if (product.quantity < 1) flag = true;
-                });
-                if(flag) newProducts = newProducts.filter(product=> product.name !== name);
-                return {...prevBill, products: newProducts};
-            });
-        };
-    };
-    const handleClick = async()=>{
-        if(!bill.billName.trim()){
-            setError("Debe agregar el nombre de la cuenta");
-            return;
-        } else if (bill.products.length < 1){
-            setError("Debe agregar productos a la cuenta");
-            return;
-        };
-        setIsLoading(true);
-        let endPoint = billID === 'nueva' ? 
-            `/api/v1/ventas/days/${bill.date}/cuentas`
-            : `/api/v1/ventas/days/${bill.date}/cuentas/${billID}/edit/products`;
-        let fetchObject = {
-            method: billID === 'nueva' ? 'POST' : 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(bill)
-        }
-        try {
-            let response = await fetch(endPoint, fetchObject);
-            let { data } = await response.json();
-            console.log(data);
-            setIsLoading(false);
-            setRedirect(true);
-        } catch (error) {
-            setIsLoading(false);
-            setError("No se pudo crear la cuenta. Intentelo de nuevo.");
-            console.error(error);
-        }
-    };
+    const [redirect, isLoading, billID, error, bill, addProduct, substractProduct, handleClick, setError, setBill] = useFetchBill(match, history);
     return (
         <main className="container pt-3">
             <div className="row">
